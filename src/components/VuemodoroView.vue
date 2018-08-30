5ea20dc70b5f2f4f3264dc0170b3c5b1b7aad81a
Richard Otvos <richard.otvos@cloudera.com>
2018-08-30 11:56:08 +0200

    Add main view component

---
+++ src/components/VuemodoroView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>What task are you working on?</h1>
+        <input type="text" v-model="taskName">
+        <p>{{taskName}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'VumemodoroView',
+        data: function () {
+            return {
+                taskName: ''
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
