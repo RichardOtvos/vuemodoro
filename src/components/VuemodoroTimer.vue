293c1a5db470065d975f3591ea55fd485aa130bb
Richard Otvos <richard.otvos@cloudera.com>
2018-09-03 10:43:53 +0200

    Hides/show start/pause according to state, and resets timer on stop

--- src/components/VuemodoroTimer.vue
+++ src/components/VuemodoroTimer.vue
@@ -2,16 +2,18 @@
     <div>
         <countdown
             class="timer"
-            v-bind:time="getPomodoroLength"
+            v-bind:time="pomodoroLength"
             v-on:countdownprogress="onCountdownProgress"
+            v-on:countdownstart="onCountdownStart"
+            v-on:countdownpause="onCountdownPause"
             v-bind:auto-start="false"
             ref="countdown"
         >
             <template slot-scope="props">{{props.minutes}}:{{props.seconds}}</template>
         </countdown>
-        <button class="start-button" v-on:click="onStartCountdown">Start</button>
-        <button class="start-button" v-on:click="onPauseCountdown">Pause</button>
-        <button class="start-button" v-on:click="onStopCountdown">Stop</button>
+        <button class="button" v-if="!isCountdownRunning" v-on:click="onStartCountdown">Start</button>
+        <button class="button" v-else v-on:click="onPauseCountdown">Pause</button>
+        <button class="button" v-on:click="onStopCountdown">Stop</button>
     </div>
 </template>
 
@@ -25,30 +27,50 @@
         name: 'VuemodoroTimer',
         components,
         props: ['pomodoroMinutes'],
-        computed: {
+        data: function () {
+            return {
+                isCountdownRunning: false,
+                shouldResetTimer: false,
+                pomodoroLength: 0
+            }
+        },
+        created: function () {
+            this.pomodoroLength = this.getPomodoroLength();
+        },
+        methods: {
+            //Countdown component emitted events
+            onCountdownProgress: function (data) {
+                document.title = `${data.minutes}:${data.seconds}`;
+            },
+            onCountdownStart: function () {
+                this.isCountdownRunning = true;
+            },
+            onCountdownPause: function () {
+                this.isCountdownRunning = false;
+            },
+
+            //Own events
+            onStartCountdown: function () {
+                this.$refs.countdown.start();
+            },
+            onPauseCountdown: function () {
+                this.$refs.countdown.pause();
+            },
+            onStopCountdown: function () {
+                this.$refs.countdown.stop();
+                this.$refs.countdown.init();
+                this.isCountdownRunning = false;
+            },
             getPomodoroLength: function () {
                 return this.pomodoroMinutes * 60 * 1000;
             }
-        },
-        methods: {
-            onCountdownProgress: function(data){
-              document.title =`${data.minutes}:${data.seconds}`;
-            },
-            onStartCountdown: function(){
-                this.$refs.countdown.start();
-            },
-            onStopCountdown: function(){
-                this.$refs.countdown.stop();
-            },
-            onPauseCountdown: function(){
-                this.$refs.countdown.pause();
-            }
+
         }
     }
 </script>
 
 <style scoped>
-    .timer{
+    .timer {
         font-size: 10em;
     }
 </style>
